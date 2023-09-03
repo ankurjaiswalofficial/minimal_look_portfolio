bebb999d4c00b2c9af082143c2e2ff89e9c6ee83
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2023-09-03 05:45:27 +0530

    overflow fix

--- static/js/script.js
+++ static/js/script.js
@@ -4,15 +4,18 @@ function menu_handler() {
     )[0];
     let backgrop_handler = nav_backdrop.children[0];
     let nav_opener = document.getElementById("nav_opener");
-    let nav_hider = document.getElementById("nav_hider");
+    let nav_hider = document.getElementById("nav_hider");let body = document.body;
     nav_opener.addEventListener("click", (e) => {
         nav_backdrop.classList.toggle("d_flex");
+        body.classList.toggle("overflow_me");
     });
     backgrop_handler.addEventListener("click", (e) => {
         nav_backdrop.classList.toggle("d_flex");
+        body.classList.toggle("overflow_me");
     });
     nav_hider.addEventListener("click", (e) => {
         nav_backdrop.classList.toggle("d_flex");
+        body.classList.toggle("overflow_me");
     });
 }
 
